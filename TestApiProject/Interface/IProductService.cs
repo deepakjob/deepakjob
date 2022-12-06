7d0531dcf1e8e34df521e5ad64ea1eb218d94d26
Deepak Singh <deepak.singh2314@gmail.com>
2022-12-06 20:07:44 +0530

    Added Async and Await

---
+++ TestApiProject/Interface/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using TestApiProject.Models;
+namespace TestApiProject
+{
+    public interface IProductService
+    {
+        bool SaveProductDetails(ProductDetails pod);
+        List<ProductDetails> SearchProductDetails(string id);
+      Task<IEnumerable<ProductDetails>> GetProductDetails();
+    }
+
+}
---
+++ TestApiProject/Service/ProductService.cs
@@ -0,0 +1,48 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TestApiProject.DBContext;
+using TestApiProject.Models;
+namespace TestApiProject.RepositoryService
+{
+    public class ProductService : IProductService
+    {
+       private readonly ProductDbContext _dbContext;
+        public ProductService(ProductDbContext dbContext)
+        { 
+        _dbContext = dbContext;
+        }
+        public  async Task<IEnumerable<ProductDetails>> GetProductDetails()
+        {
+            List<ProductDetails> li = new List<ProductDetails>();
+            var details = _dbContext.ProductDetails; 
+            if (details != null)
+            {
+                ProductDetails Prod = new ProductDetails();
+                Parallel.ForEach(details, x =>
+                {
+                    Prod.slNo = x.slNo;
+                    Prod.ProductName = x.ProductName;
+                    Prod.Price = Convert.ToInt32(x.Price);
+                    Prod.ProductDetail=x.ProductDetail;
+                    Prod.ProductType=x.ProductType;
+                    li.Add(Prod);
+
+                });
+                           
+            }
+            await Task.CompletedTask;
+            return li;
+        }
+
+        public bool SaveProductDetails(ProductDetails pod)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductDetails> SearchProductDetails(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
